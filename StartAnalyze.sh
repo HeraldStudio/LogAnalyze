c3961129686fa3fbcf83eef0fd4be63235bc7841
Eric <higuoxing@outlook.com>
2018-01-03 18:45:50 +0800

    日志文件过大导致cpu占用100%，日志接收不到问题

--- StartAnalyze.sh
+++ StartAnalyze.sh
@@ -4,4 +4,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 source /etc/profile
 
 # 将操作重定向到test.log文件中, 文件已被加入gitignore
+# 阿里云的鸡肋服务器会发生CPU 100%的问题，可以安装cpulimit进行解决，注释在这里以备不时之需
+# yum install cpulimit
+# cpulimit -l 50 /usr/bin/python3.4  main.py -d /var/log/nginx  &>>  test.log
 /usr/bin/python3.4  main.py -d /var/log/nginx  &>>  test.log
